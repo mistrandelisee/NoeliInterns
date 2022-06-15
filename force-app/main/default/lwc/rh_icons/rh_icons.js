d4e329ffac1ac86c01874ebcae278e8ab4021ae8
MistrandElisee <mistrandelisee@gmail.com>
2022-06-15 11:37:55 +0100

    bckp my icons

--- force-app/main/default/lwc/rh_icons/rh_icons.js
+++ force-app/main/default/lwc/rh_icons/rh_icons.js
@@ -15,14 +15,25 @@ const icons = {
     Search: 'utility:search',
     News: 'utility:news',
 
-    submit: 'utility:submit',
+    submit: 'utility:send',
     exportPdf: 'utility:pdf_ext',
     exportXls: 'utility:pdf_ext',
-    approve: 'utility:approve',
+    approve: 'utility:check',
     reject: 'utility:reject',
     chevrondown : 'utility:chevrondown',
     chevronup : 'utility:chevronup',
     
+    ///profile change Password
+    a_reset:'action:reset_password',
+    reset_password: 'utility:reset_password',
+    info: 'utility:info',
+    reset:"utility:skip_forward",
+    ///profile - userinfo
+    user_info: 'standard:employee_job',
+    user_info_edit: 'standard:employee_job_position',
+    more_info: 'action:contact_list',
+    ban:'utility:resource_absence',
+    promote:'utility:level_up'
 
 };
 export { icons }
